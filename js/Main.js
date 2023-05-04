f8cf72072fff2d771cac0e8b35101773353d1666
Joao-prrrr <joao.prrrr@eduge.ch>
2023-05-04 15:45:47 +0200

    jsp

--- js/Main.js
+++ js/Main.js
@@ -14,22 +14,16 @@ const messageField = document.querySelector("#messageField")
 
 
 const userName = "Lucas";
-// const receiverName = "Lucas"
+const receiverName = "Joao"
 
-window.addEventListener("locationchange", () => {
-    const path = window.location.path 
-    const receiverName = path.slice(path.indexOf("/", 2)+1)
-    
-    contactName.innerHTML = receiverName
-    
-    listMessages.forEach(message => {
-        // console.log(message)
-        if(message.sender.toLowerCase() === userName.toLowerCase() && message.receiver.toLowerCase() === receiverName.toLowerCase()){
-            messageField.innerHTML += `<li class="me">${message.message}</li>`
-        }
-        else if(message.sender.toLowerCase() === receiverName.toLowerCase() && message.receiver.toLowerCase() === userName.toLowerCase()) {
-            messageField.innerHTML += `<li class="contact">${message.message}</li>`
-        }
-        })
+contactName.innerHTML = receiverName
 
-})
+listMessages.forEach(message => {
+    // console.log(message)
+    if(message.sender.toLowerCase() === userName.toLowerCase() && message.receiver.toLowerCase() === receiverName.toLowerCase()){
+        messageField.innerHTML += `<li class="me">${message.message}</li>`
+    }
+    else if(message.sender.toLowerCase() === receiverName.toLowerCase() && message.receiver.toLowerCase() === userName.toLowerCase()) {
+        messageField.innerHTML += `<li class="contact">${message.message}</li>`
+    }
+    })
