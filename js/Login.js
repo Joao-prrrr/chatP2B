ceb8e2017bfe687d414d1dda46402b6b9b492611
lucalzon <lucas.prrsr@eduge.ch>
2023-06-01 14:50:02 +0200

    added SPA

--- js/Login.js
+++ js/Login.js
@@ -1,7 +1,14 @@
 
+document.querySelector("#openLogin").addEventListener("click", function(e){
+    e.target.style.display = "none";
+    document.querySelector("#ZoneContact").style.display = "none";
+    document.querySelector("#sectionLogin").style.display = "block";
+})
 
 
-document.querySelector("button").addEventListener("click", function(e){
+
+
+document.querySelector("#sendLogin").addEventListener("click", function(e){
     e.preventDefault()
 
     ConnectUser(document.querySelector("#username").value, document.querySelector("#password").value)
@@ -24,11 +31,13 @@ async function ConnectUser(username, password){
         const data = (await response.json()).data
 
         //const user = new User(data.id, data.username, data.token, data.pos_x, data.pos_y);
-        console.log("salut")
         const user = { "id":data.id, "username":data.username, "token":data.token, "pos_x":data.pos_x, "pos_y":data.pos_y }
         localStorage.setItem("user", JSON.stringify(user));
-
-        window.location.href = "../index.html";
+       
+        document.querySelector(" #openLogin").style.display = "block";
+        document.querySelector("#ZoneContact").style.display = "block";
+        document.querySelector("#sectionLogin").style.display = "none";
+        
 
     }
     else{
